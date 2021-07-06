6022e66c8dab71b20f717fbac489a0163d493c41
chandra <sekharc446@gmail.com>
2021-07-06 08:29:03 +0530

    Time Complexity is O(n**2) Space Complexity is O(n)

--- Array/TwoSumLeetcode/chandra-twosum.py
+++ Array/TwoSumLeetcode/chandra-twosum.py
@@ -1,30 +1,30 @@
-
-
 def twosum(nums,target):
-     if (len(nums)>=2 and len(nums)<=10000) and (target>=(-10**9) and target<=(10**9)):
-            i=0
-            while i<len(nums)-1:
-                if not((nums[i]>=pow(-10,9)) and (nums[i]<=(10**9))) or not((nums[i+1]>=(-10**9)) and (nums[i+1]<=(10**9))):
+    if (len(nums)>=2 and len(nums)<=10000) and (target>=(-10**9) and target<=(10**9)):
+        i=0
+        while i<len(nums)-1:
+            if not((nums[i]>=pow(-10,9)) and (nums[i]<=(10**9))) or not((nums[i+1]>=(-10**9)) and (nums[i+1]<=(10**9))):
          
-                    return []
-                i=i+1    
-            i=0
+                return []
+            i=i+1    
+        i=0        
+        while i<len(nums):
             j=0
             while j<len(nums):
-                
-                
-                while i<len(nums):
-                    if i==j:
-                        i=i+1
-                        continue
-                
-                    if (nums[i]+nums[j])==target:
-                        return [i,j]
-                    i=i+1
-                i=0    
+                if i==j:
+                    j=j+1
+                    continue
+                if nums[i]+nums[j]==target:
+                    return [i,j]
                 j=j+1
-     else:
-        return []        
-    
+            i=i+1
+    else:
+        return []
+print(twosum([1,2,3,3],6))                
+# Time Complexity is O(n**2)
+# Space Complexity is O(n)
+            
+            
+            
+
 
 
