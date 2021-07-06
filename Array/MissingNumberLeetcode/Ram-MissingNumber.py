ac653a57570c631f3a63675521d22788520e229f
ram677 <ramprakashreddyvnr@gmail.com>
2021-07-06 22:20:17 +0530

    Added MissingNumber Python File

---
+++ Array/MissingNumberLeetcode/Ram-MissingNumber.py
@@ -0,0 +1,32 @@
+# nums=[3,0,1]
+# def missingNumber(nums):
+#     for i in range(len(nums)+1):
+#         if i in nums:
+#             pass
+#         else:
+#             return i
+
+# print(missingNumber(nums))
+
+
+
+# nums=[3,0,1]
+# nums.sort()
+# def missingNumber(nums):
+#     for i in range(len(nums)+1):
+#         if i in nums:
+#             pass
+#         else:
+#             return i
+
+# print(missingNumber(nums))
+
+
+nums=[3,0,1]
+def missingNumber(nums):
+    n=len(nums)
+    pre_sum=sum(nums)
+    exp_sum=(n*(n+1))//2
+    return exp_sum-pre_sum
+
+print(missingNumber(nums))
