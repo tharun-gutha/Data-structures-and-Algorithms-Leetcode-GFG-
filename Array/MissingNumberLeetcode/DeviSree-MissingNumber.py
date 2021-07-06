1084b5c48e5a689034e11bab0c893fcc00f20e29
devisreenb <devisreenb@gmail.com>
2021-07-07 00:03:35 +0530

    updateed DeviSree-MissingNumber.py

--- Array/MissingNumberLeetcode/DeviSree-MissingNumber.py
+++ Array/MissingNumberLeetcode/DeviSree-MissingNumber.py
@@ -1,3 +1,5 @@
+'''
+# finding the missing element on searching
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         s=len(nums) #size of the list
@@ -9,4 +11,31 @@ class Solution:
             for i,j in zip(range(m),nums):
                 if(i!=j):
                     return i
+'''
+#finding the missing element based on difference from actual sum of numbers 
+ class Solution:
+    def missingNumber(self, nums):
+        n=len(nums)
+        actualsum=int(n*(n+1)/2)
+        listsum=sum(nums)
+        diff=actualsum-listsum
+        if(0 not in nums):
+            return 0
+        return diff
+
+li=[1,2]
+s=Solution()
+res=s.missingNumber(li)
+print(res)
+
+
+ 
+
+            
+                
+                
+               
+
+
+                    
         
