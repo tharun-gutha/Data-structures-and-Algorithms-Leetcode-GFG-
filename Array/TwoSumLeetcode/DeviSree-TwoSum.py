8320bc62f94fd0f5e04651b8abb3948c3c6ca929
devisreenb <devisreenb@gmail.com>
2021-07-07 00:39:37 +0530

    updated DeviSree-TwoSum.py

--- Array/TwoSumLeetcode/DeviSree-TwoSum.py
+++ Array/TwoSumLeetcode/DeviSree-TwoSum.py
@@ -1,9 +1,32 @@
+'''
 class Solution:
     def twoSum(self,nums,target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if(nums[i]+nums[j]==target):
                     return [i,j]
+'''
+class Solution:
+    def twoSum(self,nums,target):
+        for i in range(len(nums)):
+            search=target-nums[i]
+            if (search in nums[i+1:]):
+                j=nums.index(search)
+                if(search==nums[i]):
+                     j=nums.index(search,i+1)
+                return [i,j]
+
+
+li=[3,3]
+s=Solution()
+res=s.twoSum(li,6)
+print(res)
+
+ 
+
+            
+                
+                
 
 
  
