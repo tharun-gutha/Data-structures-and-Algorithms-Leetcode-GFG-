fb1bcdaa9c0557d6a0eb601afe178bb64e171f91
tharun-gutha <tharunky123@gmail.com>
2021-07-06 22:33:24 +0530

    updated tharun c++ solution

--- Array/MissingNumberLeetcode/tharun-missingNumber.cpp
+++ Array/MissingNumberLeetcode/tharun-missingNumber.cpp
@@ -11,3 +11,19 @@ public:
         return totalSum-arraySum;
     }
 };
+
+// to avoid overflow we solve using XOR operator
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int x1=nums[0],x2=0;
+        for(int i=1;i<n;i++){
+            x1=x1^nums[i];
+        }
+        for(int j=1;j<=n;j++){
+            x2=x2^j;
+        }
+        return (x1^x2);
+    }
+};
