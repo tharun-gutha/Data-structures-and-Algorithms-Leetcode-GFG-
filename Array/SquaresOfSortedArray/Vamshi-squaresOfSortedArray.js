5ee26355123254fe7dd8f679d5cd280960312d33
Vamshi <vv517816@gmail.com>
2021-07-08 23:01:25 +0530

    Modified squareSortedArray.js

--- Array/SquaresOfSortedArray/Vamshi-squaresOfSortedArray.js
+++ Array/SquaresOfSortedArray/Vamshi-squaresOfSortedArray.js
@@ -12,6 +12,34 @@
 };
 console.log(sortedSquares([0,2,1,5,4]));
 
+/* Time complexity O(n) */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    var len = nums.length
+    var arr = new Array(len)
+    var left = 0
+      var right = len - 1
+    for (let i = len - 1; i >= 0; i --) 
+    {
+        var leftAbs = Math.abs(nums[left]);
+        var rightAbs = Math.abs(nums[right]);
+        if (leftAbs >= rightAbs) 
+        {
+            arr[i] = leftAbs ** 2;
+            left ++
+        } 
+        else
+        {
+            arr[i] = rightAbs ** 2;
+            right --;
+        }
+    }
+    return arr;
+};
+
 /* Another method
  var sortedSquares = function(nums) {
   for(var i in nums)
