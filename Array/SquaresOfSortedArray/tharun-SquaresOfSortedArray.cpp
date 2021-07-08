8c94568c557657e523e5aa9ad633ee065adc07ee
tharun-gutha <tharunky123@gmail.com>
2021-07-08 22:39:44 +0530

    updated readme file and added comment to solution for further reference

--- Array/SquaresOfSortedArray/tharun-SquaresOfSortedArray.cpp
+++ Array/SquaresOfSortedArray/tharun-SquaresOfSortedArray.cpp
@@ -1,14 +1,17 @@
+// time complexity o(n) and space o(n)
+// use two pointer pattern to solve the problem and before submitting anaylyze some test cases where you solutin may go wrong
+//
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> v;
-        int n=nums.size();
-        if(n==1)
+        vector<int> v(nums.size());
+        int n=nums.size();//size of array
+        if(n==1)//converting negative to postive numbers
         {
             nums[0]=nums[0]*nums[0];
             return nums;
         }
-        int i=0,j=n-1;
+        int i=0,j=n-1,k=n-1;//taking two pointers and based on condition insert elements from back to front in the array
         while(i<j)
         {
             if(nums[i]<0)
@@ -17,27 +20,29 @@ public:
             }
             if(nums[i]>nums[j])
             {
-                v.push_back(nums[i]*nums[i]);
+                v[k]=(nums[i]*nums[i]);
                 i++;
+                k--;
             }
             else if(nums[i]<nums[j]){
-                v.push_back(nums[j]*nums[j]);
+                v[k]=(nums[j]*nums[j]);
                 j--;
+                k--;
             }
             else if(nums[i]==nums[j])
             {
-                v.push_back(nums[i]*nums[i]);
-                v.push_back(nums[i]*nums[i]);
+                v[k]=(nums[i]*nums[i]);
+                 k--;
+                v[k]=(nums[i]*nums[i]);
                 i++;
                 j--;
+                k--;
             }
         }
         if(j==i)
         {
-            v.push_back(nums[i]*nums[i]);
+            v[k]=(nums[i]*nums[i]);
         }
-        
-        reverse(v.begin(),v.end());
         return v;
     }
 };
