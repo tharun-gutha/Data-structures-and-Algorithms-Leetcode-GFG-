2fe9364db0e6fffab31f091e7b438b3cac45fd1c
Vamshi <vv517816@gmail.com>
2021-07-08 23:22:29 +0530

    Modified squaresOfSortedArray.py

--- Array/SquaresOfSortedArray/Vamshi-squaresOfSortedArray.py
+++ Array/SquaresOfSortedArray/Vamshi-squaresOfSortedArray.py
@@ -7,3 +7,24 @@ class Solution:
         for i in nums:
             arr.append(pow(i,2))
         return sorted(arr)
+
+
+#Another method
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        arr = [0]*len(nums)
+        left = 0
+        right = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            leftAbs = nums[left] **2
+            rightAbs = nums[right]**2
+            if leftAbs>=rightAbs:
+                arr[i]=leftAbs
+                left+=1
+            else:
+                arr[i]=rightAbs
+                right-=1
+                
+        return arr
+            
+        
