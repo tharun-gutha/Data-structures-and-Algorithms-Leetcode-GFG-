d11c1496370c1ff892b4cf99b36b537f53a57598
tharun-gutha <tharunky123@gmail.com>
2021-07-11 12:42:21 +0530

    updated readme file and completed sort an array of 0s 1s 2s

---
+++ Array/Sort-an-array-of-0s-1s-and-2s/tharun-sort-an-array-of-0s-1s-2s.cpp
@@ -0,0 +1,29 @@
+ class Solution
+ {
+   public:
+    void sort012(int a[], int n)
+    {
+        // coode here 
+        int l=0,m=0,h=n-1;
+        while(m<=h)
+        {
+            if(a[m]==0)
+            {
+                swap(a[m],a[l]);
+                l++;
+                m++;
+            }
+            else if(a[m]==1)
+            {
+                m++;
+            }
+            else if(a[m]==2)
+            {
+                swap(a[m],a[h]);
+                h--;
+            }
+        }
+        
+    }
+ }
+ 
