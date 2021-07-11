aed6da647890f3d237a1f19f403c2d970728888f
tharun-gutha <tharunky123@gmail.com>
2021-07-11 12:52:51 +0530

    added comments for further reference

--- Array/Sort-an-array-of-0s-1s-and-2s/tharun-sort-an-array-of-0s-1s-2s.cpp
+++ Array/Sort-an-array-of-0s-1s-and-2s/tharun-sort-an-array-of-0s-1s-2s.cpp
@@ -1,10 +1,11 @@
- class Solution
+ // it is an algorithm called dutch national flag . we will take three pointers low, mid and high .based on the conditon we will swap and increment the pointers. 
+class Solution
  {
    public:
     void sort012(int a[], int n)
     {
         // coode here 
-        int l=0,m=0,h=n-1;
+        int l=0,m=0,h=n-1;//take 3 pointer low mid high and based on the condition we will swap and one more thing if we are asked to solve inplace that means without extra space go for swapping.
         while(m<=h)
         {
             if(a[m]==0)
@@ -27,3 +28,5 @@
     }
  }
  
+
+ //time complexity O(n)
