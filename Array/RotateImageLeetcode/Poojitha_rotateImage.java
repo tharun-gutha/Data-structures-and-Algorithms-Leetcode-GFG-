ff5cc84fe81ee64fd47c1ad584406f76aebe48a7
G-poojitha <gollapoojitha2001@gmail.com>
2021-07-12 19:56:40 +0530

    Poojitha-rotateImage

---
+++ Array/RotateImageLeetcode/Poojitha_rotateImage.java
@@ -0,0 +1,46 @@
+METHOD 1:-
+    class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
+
+
+
+METHOD 2:-
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    } 
+        public void transpose(int[][] matrix) {
+          int n=matrix.length;
+           for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+          }    
+        }
+        public void reflect(int[][] matrix) {
+          int n = matrix.length;
+           for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+             }
+           }
+         }
+}
+            
