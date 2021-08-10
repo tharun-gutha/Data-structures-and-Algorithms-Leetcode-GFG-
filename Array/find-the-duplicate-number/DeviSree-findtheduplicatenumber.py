14e0803fd978f5d22e1010658e1fb85b1b1ba9b4
devisreenb <devisreenb@gmail.com>
2021-08-10 22:28:12 +0530

    added DeviSree-findtheduplicatenumber.py

--- Array/find-the-duplicate-number/DeviSree-findtheduplicatenumber.py
+++ Array/find-the-duplicate-number/DeviSree-findtheduplicatenumber.py
@@ -1,1 +1,13 @@
 
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for key in d:
+            if d[key]>=2:
+                return key
+# time-complexity - o(n)
